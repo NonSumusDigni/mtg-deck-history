2b2ebc9e4155ee8f6abffd4097a5c56463ef4843
Ryan <non.sumus.digni@pm.me>
2020-04-30 22:40:57 -0700

    Add script to convert .cod files to .dec

---
+++ tools/cod2dec.py
@@ -0,0 +1,41 @@
+#!/bin/bash python3
+
+import os
+import xml.etree.ElementTree as ET
+
+
+def convert_dir(dir_path):
+    for root, dirs, files in os.walk(dir_path):
+        for dir in dirs:
+            convert_dir(dir)
+
+        for file in files:
+            if file.endswith('.cod'):
+                cod_file_name = '{}/{}'.format(root, file)
+
+                file_name_part = cod_file_name[:-4]
+                dec_file_name = '{}.dec'.format(file_name_part)
+
+                # check if a .dec version already exists
+                if not os.path.isfile(dec_file_name):
+                    print('Converting file: {}...'.format(cod_file_name))
+                    dec_file_contents = convert(ET.parse(cod_file_name).getroot())
+                    with open(dec_file_name, 'w') as f:
+                        f.write(dec_file_contents)
+    
+
+def convert(xml_root):
+    output_lines = []
+    for zone in xml_root.iter('zone'):
+        zone_name = zone.get('name')
+        prefix = 'SB:' if zone_name != 'main' else ''
+        for card in zone.iter('card'):
+            card_count = card.get('number')
+            card_name = card.get('name')
+            output_lines.append('{}{} {}'.format(prefix, card_count, card_name))
+
+    return '\n'.join(output_lines)
+
+
+convert_dir(os.path.curdir)
+
